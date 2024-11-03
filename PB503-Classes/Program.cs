c6d6bbc15b71f3ae0c6df1e4a61c67f9d7271aaf
sanandev05 <senanalonegames@gmail.com>
2024-11-03 23:48:10 +0400

    Tasks completed

---
+++ PB503-Classes/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace PB503_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human();
+            human.Age = 10;
+            human.Surname = "Huseynov";
+            human.Name = "Sanan";
+            human.ShowFullData();
+        }
+    }
+
+    public class Human
+    {
+        public string Name = "Nameless"; 
+        public string Surname = "Surnameless"; 
+        public byte Age = 18; 
+
+        public Human() { 
+        Console.WriteLine("Human Created");
+         }
+
+         public Human(string name) : this()
+        {
+        Name = name;
+        }
+
+
+         public Human(string name, string surname) : this(name)
+        {
+        Surname=surname;
+        }
+
+        public Human(string name, string surname, byte age) : this(name, surname)
+        {
+            Age = age;
+        }
+
+        public void ShowFullData()
+        {
+            Console.WriteLine($"Name: {Name} Surname: {Surname} Age: {Age}");
+        }
+
+        public string GetFullData()
+        {
+            return $"Name: {Name} Surname: {Surname} Age: {Age}";
+        }
+    }
+}
---
+++ PB503-Classes/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB503_Classes
+{
+    public class Student : Human
+    {
+        public double Grant;
+
+        public Student(string name, string surname) : base(name, surname)
+        {
+        }
+       
+        public Student(string name, string surname, double grant) : this(name, surname)
+        {
+            Grant=grant;
+        }
+      
+    }
+}
